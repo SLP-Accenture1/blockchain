05370462164bea5c6fbbb04287ae7ce3786b1895
tharushi <tharushig8@gmail.com>
2021-12-14 01:19:36 +1100

    Javascript for database table done

--- frontend/Database.js
+++ frontend/Database.js
@@ -1,13 +1,27 @@
-let data = '';
+let info = '';
 
 let getFunction = () => {
-    fetch('http://192.168.0.187:3000/database')
-    //.then(response => response.json())
+    fetch('http://127.0.0.1:3000/database')
     .then(async response => {
-        data = await response.text()
-        console.log(data)
+        info = await response.json()
+        console.log(info)
+        str = document.getElementById('data').innerHTML;
+        str += '<tr>'
+        for (let key in info) {
+            str += '<td>' + info[key] + '</td>'
+        }
+        str += '</tr>'
+        console.log(info)
+        document.getElementById('data').innerHTML = str
+        console.log(str)
 
     });
+    /*Make the backend and return some dummy data
+    Frontend - change response.text to response.json
+    use for loop to iterate over the array and generate child html strings
+    set that string into the table's inner html
+    add a button (refresh) that will execute getFunction*/
+    
 }
 
-getFunction()
+getFunction();
--- frontend/test.py
+++ frontend/test.py
@@ -1,5 +1,8 @@
+import json
 from flask import Flask
+from flask.json.tag import JSONTag
 from flask_cors import CORS, cross_origin
+from json import dumps
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -7,7 +10,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route("/database")
 @cross_origin()
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return json.dumps({"Date": "16/12/2021", "Aircraft": "Airbus A380", "Tach Time (revolutions per minute)": "2400", "Total Time in Service":"30", "Name of logger": "Michael Tomm", "Certificate No. of Technician or Repair Facility": "123456", "Description of Inspections, Tests, Repairs and Alterations": "broken", "Status": "Decommissioned"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
