75935d2469408ba3cb5eff8b0c94c00b3b0f2b54
daryl <darylleesy@gmail.com>
2021-12-13 15:29:15 +1100

    added main

---
+++ backend/main.py
@@ -0,0 +1,66 @@
+from blockchain import Blockchain, load_blockchain
+from flask import Flask
+from flask_cors import CORS
+
+details = {
+    'Manufacturer' : 'Airbus',
+    'Parts' : '221839y3192',
+    'Description' : 'd',
+    'Serial Number' : '9e2h92hf3r0u0',
+    'Date of Manufacture' : '21-12-2004',
+    'Cycles since last check' : 299,
+}
+
+NAME = 'Aircraft Maintainance'
+
+
+try:
+    block1 = load_blockchain('logs/' + NAME + '.chain')
+    print('Loading Blockchain: ', NAME)
+    block1.verify()
+except FileNotFoundError:
+    print('Creating Blockchain: ', NAME)
+    block1 = Blockchain(NAME, details)
+
+print(block1)
+
+# ==============================================================
+'''
+def defaultHandler(err):
+    response = err.get_response()
+    print('response', err, err.get_response())
+    response.data = dumps({
+        "code": err.code,
+        "name": "System Error",
+        "message": err.get_description(),
+    })
+    response.content_type = 'application/json'
+    return response
+
+APP = Flask(__name__)
+CORS(APP)
+
+APP.config['TRAP_HTTP_EXCEPTIONS'] = True
+APP.register_error_handler(Exception, defaultHandler)
+
+@APP.route('/auth/login/v2', methods=['POST'])
+def login():
+    data = request.get_json()
+    auth_user_id = auth_login_v1(
+        data['email'],
+        data['password'],
+        )
+    return dumps({
+        'token' : new_token(auth_user_id),
+        'auth_user_id' : auth_user_id,
+    })
+'''
+
+# ==============================================================
+# Edit blockchain
+block1.add_cache(details)
+block1.create_block()
+
+
+
+block1.save_blockchain()
