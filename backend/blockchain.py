2a840bd7e19285f496725b6171527229954a5016
daryl <darylleesy@gmail.com>
2021-12-13 17:27:09 +1100

    resolved double entry issue

--- backend/blockchain.py
+++ backend/blockchain.py
@@ -3,20 +3,16 @@ import time
 import pickle
 import json
 
-EMPTY_ENTRY = {
-    'previous' : '',
-    'data' : [],
-    'proof' : ''
-    }
-
 class Blockchain:
-    def __init__(self, name : str, genesis_data : dict):
+    def __init__(self, name : str):
         # Initialise blockchain with genesis data
         self.name = name
         self.chain = []
-        self.cache = EMPTY_ENTRY
-        self.cache['data'].append(genesis_data)
-        self.create_block()
+        self.cache = {
+            'previous_hash' : 0,
+            'data' : [],
+            'proof' : 1
+            }
 
     def __repr__(self) -> str:
         # To print class description
@@ -24,7 +20,7 @@ class Blockchain:
 
     def __str__(self) -> str:
         # Print contents of blockchain
-        return str(self.chain)
+        return json.dumps(self.chain, sort_keys=True, indent=2)
 
     def add_cache(self, data : dict):
         data['timestamp'] = str(time.time())
@@ -32,62 +28,65 @@ class Blockchain:
 
     def create_block(self):
         # Append cached block to chain
-        if len(self.chain) == 0:
-            self.cache['previous_hash'] = 0  
-            self.cache['proof'] = 1
-        else: 
+        if len(self.chain) != 0:
             self.cache['previous_hash'] = self.hash_block(self.chain[-1])
-            self.cache['proof'] = self.mine_proof()
+        self.mine_proof(self.cache)
 
-        self.chain.append(self.cache)
+        self.chain.append(self.cache.copy())
         self.broadcast()
 
         # Clear cache
-        self.cache = EMPTY_ENTRY
+        self.cache['data'] = []
+        self.cache.clear()
+        self.cache = {
+            'previous_hash' : 0,
+            'data' : [],
+            'proof' : 1
+            }
 
     def hash_block(self, block):
         encoded_block = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
     
-    def mine_proof(self):
-        data = self.chain[-1]['proof']
-        nonce = 1
+    def mine_proof(self, block : dict):
+        data = self.chain[-1]['proof'] if len(self.chain) > 0 else 0
+        block['proof'] = 1
         check_proof = False
 
         while check_proof is False:
-            hash_operation = hashlib.sha256(str(nonce**2 - data**2).encode()).hexdigest()
-            print('Trying ', nonce, ': ', hash_operation)
-            if hash_operation[:3] == '000':
+            block['proof']**2 - data**2
+            hash_operation = self.hash_block(block)
+            # print('Trying ', block['proof'], ': ', hash_operation)
+            if hash_operation[:2] == '00':
                 check_proof = True
             else:
-                nonce += 1
+                block['proof'] += 1
         
-        return nonce
 
     def save_blockchain(self):
         with open('logs/' + self.name + '.bc', 'wb') as f:
             pickle.dump(self, f)
 
-    def verify(self):
-        previous_block = self.chain[0]
-        i = 1
+    # def verify(self):
+    #     previous_block = self.chain[0]
+    #     i = 1
 		
-        while i < len(self.chain):
-            block = self.chain[i]
-            if block['previous_hash'] != self.hash(previous_block):
-                return False
+    #     while i < len(self.chain):
+    #         block = self.chain[i]
+    #         if block['previous_hash'] != self.hash(previous_block):
+    #             return False
 			
-            previous_proof = previous_block['proof']
-            proof = block['proof']
-            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2)).encode().hexdigest()
+    #         previous_proof = previous_block['proof']
+    #         proof = block['proof']
+    #         hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2)).encode().hexdigest()
 			
-            if hash_operation[:4] != '00000':
-                raise Exception('Blockchain has been tampered')
-            previous_block = block
-            i += 1
+    #         if hash_operation[:4] != '00000':
+    #             raise Exception('Blockchain has been tampered')
+    #         previous_block = block
+    #         i += 1
 		
-        print('Blockchain is verified')
-        return True
+    #     print('Blockchain is verified')
+    #     return True
 
     def broadcast(self):
         pass
--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from blockchain import Blockchain, load_blockchain
 from flask import Flask
 from flask_cors import CORS
 
-details = {
+details1 = {
     'Manufacturer' : 'Airbus',
     'Parts' : '221839y3192',
     'Description' : 'd',
@@ -11,16 +11,25 @@ details = {
     'Cycles since last check' : 299,
 }
 
+details2 = {
+    'Manufacturer' : 'Boeing',
+    'Parts' : '221839y3192',
+    'Description' : 'd',
+    'Serial Number' : '9e2h92hf3r0u0',
+    'Date of Manufacture' : '21-12-2005',
+    'Cycles since last check' : 299,
+}
+
 NAME = 'Aircraft Maintainance'
 
 
 try:
-    block1 = load_blockchain('logs/' + NAME + '.chain')
+    block1 = load_blockchain('logs/' + NAME + '.bc')
     print('Loading Blockchain: ', NAME)
-    block1.verify()
+    # block1.verify()
 except FileNotFoundError:
     print('Creating Blockchain: ', NAME)
-    block1 = Blockchain(NAME, details)
+    block1 = Blockchain(NAME)
 
 print(block1)
 
@@ -58,9 +67,9 @@ def login():
 
 # ==============================================================
 # Edit blockchain
-block1.add_cache(details)
+block1.add_cache(details2)
 block1.create_block()
 
-
+print(block1)
 
 block1.save_blockchain()
